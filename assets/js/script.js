34693f98457e594873a940d4238329ee5da366f8
Tiago Paludo <tiagop-d@hotmail.com>
2024-06-05 11:28:18 +0000

    add function on JS to count heads and tail

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
     const flipButton = document.getElementById("flip-button");
     const result = document.getElementById("result");
+    const headsCountElement = document.getElementById("headsCount");
+    const tailsCountElement = document.getElementById("tailsCount");
+
+    let headsCount = 0;
+    let tailsCount = 0;
 
     flipButton.addEventListener("click", function() {
         const outcome = Math.random() < 0.5 ? "Heads" : "Tails";
         result.textContent = outcome;
+
+        if (outcome === "Heads") {
+            headsCount++;
+            headsCountElement.textContent = headsCount;
+        } else {
+            tailsCount++;
+            tailsCountElement.textContent = tailsCount;
+        }
     });
 
 });
